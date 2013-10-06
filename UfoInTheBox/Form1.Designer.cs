f6989be8558f30d0f028d65b1a786b631f9e3f97
Tatu Solla <tatu.solla@live.com>
2013-10-07 02:19:47 +0300

    Saa nähä mitä tässä käy

---
+++ UfoInTheBox/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace UfoInTheBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bg2 = new System.Windows.Forms.PictureBox();
+            this.background = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bg2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(799, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stopToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // bg2
+            // 
+            this.bg2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.bg2.BackgroundImage = global::UfoInTheBox.Properties.Resources.BackgroundPart2;
+            this.bg2.Location = new System.Drawing.Point(800, 25);
+            this.bg2.Name = "bg2";
+            this.bg2.Size = new System.Drawing.Size(800, 600);
+            this.bg2.TabIndex = 2;
+            this.bg2.TabStop = false;
+            // 
+            // background
+            // 
+            this.background.BackColor = System.Drawing.SystemColors.InfoText;
+            this.background.BackgroundImage = global::UfoInTheBox.Properties.Resources.BackgroundPart1;
+            this.background.Location = new System.Drawing.Point(0, 25);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(800, 600);
+            this.background.TabIndex = 1;
+            this.background.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(799, 624);
+            this.Controls.Add(this.bg2);
+            this.Controls.Add(this.background);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(815, 662);
+            this.MinimumSize = new System.Drawing.Size(815, 662);
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bg2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.PictureBox background;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.PictureBox bg2;
+    }
+}
+
---
+++ UfoInTheBox/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UfoInTheBox
+{
+    public partial class Form1 : Form
+    {
+        Thread bg;
+        bool flag = false;
+        Graphics g;
+
+        private delegate void SetBackgroundPosition(int x, int y, int bgNum);
+       
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+            bg = new Thread(new ThreadStart(bgProcedure));
+            bg.Start();
+        }
+
+        public void SetBackgroundPositionDelegate(int x, int y, int bgNum)
+        {
+            switch (bgNum)
+            {
+                case 1:
+                    this.background.Location = new Point(x, y);
+                    break;
+                case 2:
+                    this.bg2.Location = new Point(x, y);
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+
+        public void bgProcedure()
+        {
+
+            while (!flag)
+            {
+
+                const int MOVE_BG_X = 1;
+                SetBackgroundPosition m = SetBackgroundPositionDelegate;
+                int BG1currentLocation = background.Location.X;
+
+                if (BG1currentLocation == 0)
+                {
+                    m.Invoke(799, 25,2);
+                }
+
+                m.Invoke(BG1currentLocation - MOVE_BG_X, background.Location.Y, 1);
+
+                int BG2currentLocation = bg2.Location.X;
+
+                if (BG2currentLocation == 0)
+                {
+                    m.Invoke(780, 25,1);
+                }
+                m.Invoke(BG2currentLocation - MOVE_BG_X, bg2.Location.Y,2);
+
+                Thread.Sleep(10);
+            }
+        }
+
+        
+
+        
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    
+                    break;
+            }
+        }
+
+        #region Exits and stops
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            flag = true;
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!flag)
+            {
+                flag = true;
+            }
+            Thread.Sleep(100);
+            Application.Exit();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!flag)
+            {
+                flag = true;
+            }
+            Thread.Sleep(100);
+            Application.Exit();
+        }
+        #endregion
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g.DrawImage(Properties.Resources.Ufo, 100, 100);
+        }
+
+    }
+}
