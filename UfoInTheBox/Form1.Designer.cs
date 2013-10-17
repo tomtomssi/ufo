20865ca07564872d36cca7193c4668a330ae4a85
Tatu Solla <tatu.solla@live.com>
2013-10-18 01:52:15 +0300

    valmis

--- UfoInTheBox/Form1.Designer.cs
+++ UfoInTheBox/Form1.Designer.cs
@@ -35,7 +35,6 @@
             this.bg2 = new System.Windows.Forms.PictureBox();
             this.background = new System.Windows.Forms.PictureBox();
             this.ufoBox = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bg2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
@@ -104,22 +103,12 @@
             this.ufoBox.TabIndex = 3;
             this.ufoBox.TabStop = false;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(128, 501);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.ClientSize = new System.Drawing.Size(799, 624);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.ufoBox);
             this.Controls.Add(this.bg2);
             this.Controls.Add(this.background);
@@ -151,7 +140,6 @@
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.PictureBox bg2;
         private System.Windows.Forms.PictureBox ufoBox;
-        private System.Windows.Forms.Label label1;
     }
 }
 
